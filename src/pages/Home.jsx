7e3797e97ac312e42335bd400b0d67a63a125b0f
EstyGlez <estibalizzmakeup@gmail.com>
2024-02-22 16:00:01 +0100

    created top5productos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,12 @@
+import React from "react";
+import Top5products from "../components/addProductsForm/top5products/Top5products";
+
+
+
 export const Home = () => {
     return (
-        <div>Home</div>
+        <>
+        <Top5products/>
+        </>
     )
 }
