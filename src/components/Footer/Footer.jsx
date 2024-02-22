bff61a75f13fc6a0ad073b011b8abcf327020236
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2024-02-22 21:25:56 +0100

    footer navegation

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,16 +8,16 @@ export const Footer = () => {
         <div className="footer-section">
           <h3 className='footerTitle'>MOON EXPRESS</h3>
           <ul>
-            <li><a href="#">Inicio</a></li>
-            <li><a href="#">Productos</a></li>
-            <li><a href="#">Nosotros</a></li>
-            <li><a href="#">Contacto</a></li>
+            <li><a href="/">Inicio</a></li>
+            <li><a href="/AvailableProducts">Productos</a></li>
+            <li><a href="/AboutUs">Nosotros</a></li>
+            <li><a href="/Contact">Contacto</a></li>
           </ul>
         </div>
         <div className="footer-section">
           <h3 className='footerTitle'>Contacto</h3>
-          <p>Teléfono: +123 456 789</p>
-          <p>Email: info@moonexpress.com</p>
+          <p className='contctInfo'>Teléfono: +123 456 789</p>
+          <p className='contctInfo'>Email: info@moonexpress.com</p>
         </div>
         <div className="footer-section">
           <h3 className='footerTitle'>Síguenos</h3>
