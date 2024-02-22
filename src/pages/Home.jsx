2b30613fa0162f1e8c85041536984c85feab9cd4
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2024-02-22 21:16:17 +0100

    styling footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,19 @@
-import React from "react";
+// import React from "react";
+import Stack from 'react-bootstrap/Stack';
 import Top5products from "../components/addProductsForm/top5products/Top5products";
 import Brands from "../components/brands/Brands.jsx";
-import Footer from "../components/Footer/Footer.jsx";
-
-
-
+// import Footer from "../components/Footer/Footer.jsx";
+import "./Home.css"
 
 export const Home = () => {
     return (
-        <>
+        <Stack gap={5} className='homeBody'>
         <Top5products/>
+        <br></br>
+        <br></br>
         <Brands/>
-        <Footer/>
-        </>
+
+        </Stack>
         
     )
 }
